41b4a4d623fc5d95aabf23546f7819f1e2a9ba64
Daniel Sousa <daniel.s@crowdbotics.com>
2024-01-18 17:30:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-comp-install-66",
+  url: "https://tst-comp-install-66.botics.co",
+  api: "https://tst-comp-install-66.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
